f5f139e170d993798420a0e92e7a8f11eb5817a7
Anand Ray <a2ray@ucsd.edu>
2020-04-19 18:25:21 +1000

    WIP pairwise, colwise for nonstat

--- GP.jl
+++ GP.jl
@@ -68,14 +68,14 @@ function GPfit(ytrain, xtrain, xtest, λtest::Array{Float64,2}, λtrain::Array{F
         my = mean(ytrain)
     end
     ytrain = ytrain .- my
-    Kstar = GP.makekernel(xtrain, xtest, λtest, λtrain, p)
-    K_y = GP.makekernel(xtrain, xtrain, λtrain, λtrain, p) + Matrix(δ^2*I, (size(xtrain,2)), (size(xtrain,2)))
+    Kstar = GP.meshkernel(xtrain, xtest, λtest, λtrain, p)
+    K_y = GP.meshkernel(xtrain, xtrain, λtrain, λtrain, p) + Matrix(δ^2*I, (size(xtrain,2)), (size(xtrain,2)))
     U = cholesky(K_y).U
-    ytest = my .+ Kstar'*(U\(U'\ytrain))
+    ytest = my .+ Kstar*(U\(U'\ytrain))
     var_prior, var_y = [],[]
     if !nogetvars
-        var_prior = GP.makekernel(xtest, xtest, λtest, λtest, p)
-        var_y =  var_prior - Kstar'*(U\(U'\Kstar))
+        var_prior = GP.meshkernel(xtest, xtest, λtest, λtest, p)
+        var_y =  var_prior - Kstar*(U\(U'\Kstar'))
     end
     ytest, var_y, var_prior
 end
@@ -86,18 +86,46 @@ function getPI(y::Array{Float64, 1}, fₓ::Array{Float64, 1}, σₓ²::Array{Flo
     return ccdf.(d, X)
 end
 
-function nonstatkernel(xtrain, xtest)
-    nrows = size(xtrain, 2)
-    ncols = size(xtest, 2)
-    ϕ = zeros(nrows, ncols)
-    nd = size(λtrain, 1)
-    for i = 1:nrows, j = 1:ncols
-        ϕ[i,j] = 2^(nd/2)*
-                (det(diagm(0=>λtrain[:,i].^2))*det(diagm(0=>λtest[:,j].^2)))^0.25 *
-                det(diagm(0=>(λtrain[:,i].^2 + λtest[:,j].^2)))^-0.5 *
-                exp(-0.5*((sqrt(2)*norm((xtrain[:,i] - xtest[:,j])./(sqrt.(λtrain[:,i].^2 + λtest[:,j].^2)),p))^p))
-    end
+function meshkernel(xtrain::AbstractArray, xtest::AbstractArray,
+                    λtest::AbstractArray, λtrain::AbstractArray, p)
+    nrows = size(xtest, 2)
+    ncols = size(xtrain, 2)
+        ϕ = [kernel(xtrain[:,j], xtest[:,i], λtrain[:,j], λtest[:,i]) for i = 1:nrows, j = 1:ncols]
     return ϕ
+end
+
+function mapkernel(xtrain::AbstractArray, xtest::AbstractArray,
+                    λtest::AbstractArray, λtrain::AbstractArray; p=2)
+    nrows = size(xtest, 2)
+    ncols = size(xtrain, 2)
+    ϕ = Array{Float64, 2}(undef, nrows, ncols)
+    map!(x->x, ϕ, pairwise(xtrain, xtest, λtrain, λtest))
+end
+
+function colwise(xtrain::AbstractArray, xtest::AbstractArray,
+                 λtrain::AbstractArray, λtest::AbstractArray)
+    na, nb = size(xtrain, 2), size(xtest, 2)
+    @assert min(na, nb) == 1
+    if na == 1
+        a, b, la, lb = xtrain, xtest, λtrain, λtest
+    else
+        a, b, la, lb = xtest, xtrain, λtest, λtrain
+    end
+    [kernel(a, la, b[:,j], lb[:,j]) for j = 1:max(na, nb)]
+end
+
+function pairwise(xtrain, xtest, λtrain, λtest)
+    nrows = size(xtest, 2)
+    ncols = size(xtrain, 2)
+    [kernel(xtrain[:,j], xtest[:,i], λtrain[:,j], λtest[:,i]) for i = 1:nrows, j = 1:ncols]
+end
+
+function kernel(xtrain::AbstractArray, xtest::AbstractArray,
+                λtest::AbstractArray, λtrain::AbstractArray; p=2)
+    avλ² = 0.5*(λtrain.^2 + λtest.^2)
+    det(diagm(0=>(λtrain.*λtest).^2))^0.25 *
+    det(diagm(0=>(avλ²)))^-0.5 *
+    exp(-0.5*((norm((xtrain - xtest)./sqrt.(avλ²),p))^p))
 end
 
 end
