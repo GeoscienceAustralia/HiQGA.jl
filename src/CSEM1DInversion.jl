e4671637a738241d5267f73b3c8aecf1d2b5fd04
Anand Ray <a2ray@ucsd.edu>
2020-06-05 20:45:57 +1000

    WIP CSEMinversion setup z

--- src/CSEM1DInversion.jl
+++ src/CSEM1DInversion.jl
@@ -6,25 +6,38 @@ using TransD_GP, PyPlot, LinearAlgebra, CommonToAll
 export CSEMRadialEr
 
 mutable struct CSEMRadialEr<:Operator1D
-    d      :: Array{Float64}
-    useML  :: Bool
-    σ      :: Float64
-    F      :: RadialEr
-    r      :: Array{ComplexF64}
     d      :: Array{ComplexF64}
+    useML  :: Bool
+    σ      :: Array{Float64}
+    F      :: RadialEr
     z      :: Array{Float64, 1}
+    nfixed :: Int
     ρ      :: Array{Float64, 1}
+    select :: Array{Bool}
+    ndata  :: Array{Int, 1}
 end
 
-function CSEMRadialEr(d::Array{Float64}, zfixed, zsetup, ρfixed;
-                        zTx = 975.
-                        zRx = 1000.,
-                        rRx = collect(500:32:5000)
-                        RxAzim = 0.,
-                        TxDip = 0.,
-                        nmax = 200,
-                        useML=false,
-                        σ=1.0)
+function CSEMRadialEr(d::Array{Float64},
+                    zfixed::Array{Float64, 1},
+                    ρfixed::Array{Float64, 1};
+                    zTx = 975.,
+                    zRx = 1000.,
+                    rRx = collect(500:32:5000),
+                    RxAzim = 0.,
+                    TxDip = 0.,
+                    nmax = 200,
+                    useML=false,
+                    σ=[1.0])
+    @assert length(zfixed) == length(ρfixed)
+    z = [zfixed, zsetup...]
+    @assert all(diff(z).>0)
+    @assert nmax >= length(z)
+    @assert size(σ) == size(d)
+    ρ = zeros(length(z))
+    nfixed = length(ρfixed)
+    ndata = permutedims(sum(.!isnan.(d[:,ifreq])))
+    select = .!isnan(d)
+    ρ[1:nfixed] .= ρfixed
     F = CSEM1DEr.RadialErLagged(  zTx    = [zTx],
                                   rRx    = rRx,
                                   freqs  = freqs,
@@ -32,23 +45,23 @@ function CSEMRadialEr(d::Array{Float64}, zfixed, zsetup, ρfixed;
                                   RxAzim = RxAzim,
                                   TxDip  = TxDip,
                                   nmax   = nmax)
-    z = [zfixed, zsetup]
-    ρ = [ρfixed, zeros(nmax)...]
-    CSEMRadialEr(d, useML, σ, F, zeros(ComplexF64, size(F.Er), z, ρ)
+    CSEMRadialEr(d, useML, σ, F, z, nfixed, ρ, select, ndata)
 end
 
 function get_misfit(m::TransD_GP.Model, opt::TransD_GP.Options, csem::CSEMRadialEr)
     chi2by2 = 0.0
     if !opt.debug
-        d, Er = csem.d, csem.F.Er
-        select = .!isnan.(d)
-        getfield!(csem.F, z::Array{Float64, 1}, ρ::Array{Float64, 1})
-        csem.r = Er[select] - d[select]
-        if line.useML
-            N = sum(select)
-            chi2by2 = 0.5*N*log(norm(r)^2)
-        else
-            chi2by2 = r'*r/(2line.σ^2)
+        csem.ρ[csem.nfixed+1:end] .= 10 .^m.fstar
+        d, Er, select, ndata, σ = csem.d, csem.F.Er, csem.select, csem.ndata, csem.σ
+        getfield!(csem.F, csem.z, csem.ρ)
+        @views for ifreq in eachindex(csem.F.freqs)
+            r, d, s, idx = Er[:,ifreq], d[:,ifreq], σ[:,ifreq], select[:,ifreq]
+            r .= (r - d)./s
+            if csem.useML
+                chi2by2 += ndata[ifreq]*log(norm(r[idx])^2)
+            else
+                chi2by2 += norm(r[idx])^2
+            end
         end
     end
     return chi2by2
