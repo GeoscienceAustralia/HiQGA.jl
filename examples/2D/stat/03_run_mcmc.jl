cd8c88addec71666280aa75be12eadd4bd234b68
Anand Ray <a2ray@ucsd.edu>
2020-07-13 18:03:31 +1000

    changes to image run scripts

--- examples/2D/stat/03_run_mcmc.jl
+++ examples/2D/stat/03_run_mcmc.jl
@@ -3,6 +3,7 @@ nsamples, nchains, nchainsatone = 50001, 8, 1
 Tmax = 2.50
 addprocs(nchains)
 @info "workers are $(workers())"
+@everywhere any($srcdir .== LOAD_PATH) || push!(LOAD_PATH, $srcdir)
 @everywhere any(pwd() .== LOAD_PATH) || push!(LOAD_PATH, pwd())
 @everywhere using Distributed
 @everywhere import MCMC_Driver
