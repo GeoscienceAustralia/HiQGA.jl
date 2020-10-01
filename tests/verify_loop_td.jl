2018bd43ad4a0a3a31657344f4dd64a7b5a383a8
Anand Ray <a2ray@ucsd.edu>
2020-10-01 21:55:55 +1000

    circular loop test is also ok

--- tests/verify_loop_td.jl
+++ tests/verify_loop_td.jl
@@ -12,7 +12,7 @@ zRx = -0.001
 zTx = -0.002
 doconvramp = false
 modelprimary = true
-nkᵣeval = 100
+nkᵣeval = 200
 ntimesperdecade = 15
 nfreqsperdecade = 15
 times = 10 .^LinRange(-6,1,40)
@@ -38,8 +38,17 @@ dbzdt = -1 ./(sig0*a^3)*(3*erf.(theta*a) - 2/sqrt(pi)*theta.*a.*(3 .+ 2*theta.^2
 dbzdt = dbzdt/(pi*a^2)
 ##
 figure()
+s1 = subplot(211)
 loglog(F.interptimes,4*pi*1e-7*abs.(F.HTDinterp), label="circle W&H")
 loglog(F.interptimes,abs.(dbzdt), "--", label="circle Christiansen")
 xlim(extrema(times))
-grid()
+ylabel("dBzdt V/Am^4")
+grid(which="both")
 legend()
+s2 = subplot(212, sharex=s1)
+# loglog(F.interptimes, 100*abs.(-AEM_VMD_HMD.μ*F.HTDinterp-dbzdt)./abs.(dbzdt))
+# signs are supposed to be the same at loop centre ...
+loglog(F.interptimes, 100*abs.(abs.(AEM_VMD_HMD.μ*F.HTDinterp)-abs.(dbzdt))./abs.(dbzdt))
+grid(which="both")
+ylabel("% difference")
+xlabel("time s")
