aee0bf1f7c34de895f2d42b3ec45cf02b1dfd3e8
Anandaroop Ray <ar0754@gadi-login-06.gadi.nci.org.au>
2020-10-10 14:41:56 +1100

    gadi 90% cpu uti; mpi

--- examples/SkyTEM1D/SSC_resistive/submit.sh
+++ examples/SkyTEM1D/SSC_resistive/submit.sh
@@ -3,11 +3,11 @@
 #PBS -q normal
 #PBS -l ncpus=4
 #PBS -l mem=16GB
-#PBS -l walltime=01:40:00
+#PBS -l walltime=01:00:00
 #PBS -l wd
 #PBS -N juliaAEM
 #PBS -e grid.err
 
 cd /scratch/z67/ar/transD_GP/examples/SkyTEM1D/SSC_resistive
 module load openmpi
-mpirun -np 4 /home/547/ar0754/bin/julia fullscript.jl >& outfile
+/home/547/ar0754/bin/julia fullscript.jl >& outfile
