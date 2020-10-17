70c68dbd27d1eeccc3c1587cfd6665edfe7b493f
Anandaroop Ray <ar0754@gadi-login-01.gadi.nci.org.au>
2020-10-17 14:43:03 +1100

    works on 1200 cores

--- examples/SkyTEM1D/Menindee/line_data/submit.sh
+++ examples/SkyTEM1D/Menindee/line_data/submit.sh
@@ -3,11 +3,11 @@
 #PBS -q normal
 #PBS -l ncpus=1200
 #PBS -l mem=4800GB
-#PBS -l walltime=15:00:00
+#PBS -l walltime=12:00:00
 #PBS -l wd
 #PBS -N juliaAEM
 #PBS -e grid.err
 
-/home/anray/Software/transD_GP/examples/SkyTEM1D/Menindee/line_data
+cd /scratch/z67/ar/transD_GP/examples/SkyTEM1D/Menindee/line_data
 module load openmpi
-/home/547/ar0754/bin/julia doall.jl >& outfile
+mpirun -np 1200 /home/547/ar0754/bin/julia doall.jl >& outfile
