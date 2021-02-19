63c9db7e84082fe451bc87e677d4d2775d3d30f8
Anandaroop Ray <a2ray@ucsd.edu>
2021-02-19 18:50:04 +1100

    credible nuisance inversion out to 100K

--- examples/tempest/nuisance/04_plot_results.jl
+++ examples/tempest/nuisance/04_plot_results.jl
@@ -3,7 +3,7 @@ gcf()
 ##
 opt.xall[:] .= zall
 transD_GP.plot_posterior(tempest, opt, burninfrac=0.5, figsize=(4,4),
-    cmappdf="inferno", qp1=0.01, qp2=0.99)
+    cmappdf="inferno")
 step(log10.(ρ[2:end]), z[2:end], color="w", alpha=0.4)
 gca().invert_xaxis()
 ## nuisance histograms
