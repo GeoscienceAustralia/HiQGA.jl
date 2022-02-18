0316dc911904a75362603b51b122d2ab4faa0d2b
Anand Ray <a2ray@ucsd.edu>
2022-02-18 12:44:39 +1100

    WIP joint

---
+++ examples/AEM_MT_joint/AEM_only/02_make_aem_inversion_opts_s.jl
@@ -0,0 +1,35 @@
+using transD_GP, Distributed
+## make options for the stationary GP
+fileprefix = "OrstUhn_"
+K = transD_GP.GP.OrstUhn()
+nmin, nmax = 2, 40
+fbounds = [-0.5 4]
+sdev_pos = [0.05abs(diff([extrema(znall)...])[1])]
+sdev_prop = 0.07*diff(fbounds, dims=2)[:]
+demean = false
+sdev_dc = 0.008*diff(fbounds, dims=2)[:]
+sampledc = true
+xall = permutedims(collect(znall))
+xbounds = permutedims([extrema(znall)...])
+λ, δ = [2], 0.1
+useML = false
+aem.useML = useML
+## Initialize a stationary GP using these options
+Random.seed!(12)
+opt = transD_GP.OptionsStat(nmin = nmin,
+                        nmax = nmax,
+                        xbounds = xbounds,
+                        fbounds = fbounds,
+                        fdataname = fileprefix,
+                        xall = xall,
+                        λ = λ,
+                        δ = δ,
+                        sdev_prop = sdev_prop,
+                        sdev_pos = sdev_pos,
+                        save_freq = 50,
+                        demean = demean,
+                        sampledc = sampledc,
+                        sdev_dc = sdev_dc,
+                        quasimultid = false,
+                        K = K
+                        )
