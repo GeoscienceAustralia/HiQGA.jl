55f73f262bfe6b3661ca3668bf41d54cd0f072e7
Anandaroop Ray <a2ray@ucsd.edu>
2022-03-13 00:40:30 +1100

    plotting changes

--- examples/tempest/amponly/04_plot_results.jl
+++ examples/tempest/amponly/04_plot_results.jl
@@ -1,8 +1,8 @@
 transD_GP.getchi2forall(opt)
 ##
 opt.xall[:] .= zall
-transD_GP.plot_posterior(tempest, opt, burninfrac=0.5, figsize=(10,6),
-    cmappdf="cividis", qp1=0.05, qp2=0.95, nbins=200, vmaxpc=1)
+transD_GP.plot_posterior(tempest, opt, burninfrac=0.5, figsize=(10,6), cmappdf="hot",
+        qp1=0.05, qp2=0.95, nbins=200, vmaxpc=1)
 ax = gcf().axes
 ax[1].invert_xaxis()
 ax[1].step(log10.(ρ[2:end]), z[2:end], color="r", alpha=0.5)
