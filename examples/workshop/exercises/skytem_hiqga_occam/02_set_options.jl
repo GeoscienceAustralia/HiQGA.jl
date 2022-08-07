3c2cb80353521d8d7df68758eef1d056f05618f6
Anand Ray <a2ray@ucsd.edu>
2022-08-07 14:26:04 +1000

    upto menindee occam wkshp works

---
+++ examples/workshop/exercises/skytem_hiqga_occam/02_set_options.jl
@@ -0,0 +1,37 @@
+## make gradientinv options same for all soundings
+σstart, σ0         = -2, -2
+zfixed             = [-1e5]
+ρfixed             = [1e12]
+zstart             = 0.0
+extendfrac, dz     = 1.06, 1.25
+nlayers            = 52
+ρbg                = 10 # test linear resistivity for forward model before inversion
+regtype            = :R1
+nstepsmax          = 4 # total iterations of Occam (outer)
+ntries             = 6 # tries per outer iteration
+target             = nothing
+lo                 = -2.5
+hi                 = 0.5
+λ²min              = -0.5
+λ²max              = 8
+β²                 = 0.1
+knownvalue         = 0.7 # exit inner loop at this much misfit reduction
+breakonknown       = true # exit inner loop at knownvalue
+showgeomplot       = true # show model discretization
+## plot n random soundings and a background response
+using Random
+nplot = 1
+nplot = min(nplot, length(soundings))
+opt = transD_GP.SkyTEM1DInversion.makeoperatorandoptions(
+                            soundings,
+                            rseed = 2,
+                            zfixed = zfixed,
+                            ρfixed = ρfixed,
+                            zstart = zstart,
+                            extendfrac = extendfrac,
+                            dz = dz,
+                            ρbg = ρbg,
+                            nlayers = nlayers,
+                            nplot = nplot,
+                            showgeomplot = showgeomplot)
+
---
+++ examples/workshop/exercises/skytem_hiqga_occam/03_parallel_run.jl
@@ -0,0 +1,34 @@
+using Distributed
+addprocs(4) # number of parallel cores
+# split into sequential iterations of parallel soundings
+nsoundings = length(soundings)
+ncores = nworkers()
+nsequentialiters = ceil(Int, nsoundings/ncores)
+@info "will require $nsequentialiters iterations of $ncores soundings"
+## set up transD_GP
+@everywhere using Distributed
+@everywhere using HiQGA.transD_GP
+## do the parallel soundings
+@info "starting"
+transD_GP.SkyTEM1DInversion.loopacrosssoundings(soundings, σstart, σ0,
+                            nsequentialiters   = nsequentialiters,              
+                            zfixed             = zfixed          ,              
+                            ρfixed             = ρfixed          ,              
+                            zstart             = zstart          ,              
+                            extendfrac         = extendfrac      ,              
+                            dz                 = dz              ,              
+                            ρbg                = ρbg             ,              
+                            nlayers            = nlayers         ,                                 
+                            regtype            = regtype         ,              
+                            nstepsmax          = nstepsmax       ,              
+                            ntries             = ntries          ,              
+                            target             = target          ,              
+                            lo                 = lo              ,              
+                            hi                 = hi              ,              
+                            λ²min              = λ²min           ,              
+                            λ²max              = λ²max           ,              
+                            β²                 = β²              ,
+                            knownvalue         = knownvalue      ,              
+                            breakonknown       = breakonknown    ,              
+                            )                  
+rmprocs(workers())
