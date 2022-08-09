7a519b0070b71eecd09018c953d0c4ef890654bf
Anand Ray <a2ray@ucsd.edu>
2022-08-09 10:40:36 +1000

    smaller figures for laptops

--- examples/workshop/exercises/skytem_hiqga_occam/04_plot.jl
+++ examples/workshop/exercises/skytem_hiqga_occam/04_plot.jl
@@ -1,4 +1,4 @@
 dxplot, dzplot = 5, 2
 transD_GP.SkyTEM1DInversion.splitlineconvandlast(soundings, dxplot, dzplot; zstart=zstart, extendfrac=extendfrac, dz=dz, nlayers=nlayers, 
-    figsize=(12,7), vmin=lo, vmax=hi, postfix="_β²_$(β²)_$(regtype)_bg_$(10. ^σ0)Spm", 
-    preferEright=true, showplot=true, logscale=true)
+    figsize=(8.5,5), vmin=lo, vmax=hi, postfix="_β²_$(β²)_$(regtype)_bg_$(10. ^σ0)Spm", 
+    preferEright=true, showplot=true, logscale=true, fontsize=9, yl=[-250,80])
--- examples/workshop/worked_out/skytem_hiqga_occam/04_plot.jl
+++ examples/workshop/worked_out/skytem_hiqga_occam/04_plot.jl
@@ -1,3 +1,3 @@
-transD_GP.SkyTEM1DInversion.splitlineconvandlast(soundings, 5, 2; zstart=zstart, extendfrac=extendfrac, dz=dz, nlayers=nlayers, 
-    figsize=(12,7), vmin=lo, vmax=hi, postfix="_β²_$(β²)_$(regtype)_bg_$(10. ^σ0)Spm", 
-    preferEright=true, showplot=true, logscale=true)
+transD_GP.SkyTEM1DInversion.splitlineconvandlast(soundings, dxplot, dzplot; zstart=zstart, extendfrac=extendfrac, dz=dz, nlayers=nlayers, 
+    figsize=(8.5,5), vmin=lo, vmax=hi, postfix="_β²_$(β²)_$(regtype)_bg_$(10. ^σ0)Spm", 
+    preferEright=true, showplot=true, logscale=true, fontsize=9, yl=[-250,80])
