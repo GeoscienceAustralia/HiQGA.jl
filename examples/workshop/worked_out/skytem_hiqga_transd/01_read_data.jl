2e26106b0c00c73d34754abaa556c077877392c3
Anand Ray <a2ray@ucsd.edu>
2022-08-09 11:23:42 +1000

    smaller figures for laptops

--- examples/workshop/worked_out/skytem_hiqga_transd/01_read_data.jl
+++ examples/workshop/worked_out/skytem_hiqga_transd/01_read_data.jl
@@ -36,4 +36,4 @@ soundings = transD_GP.SkyTEM1DInversion.read_survey_files(fname_dat = fname_dat,
 									 relerror         = relerror,
 									 units            = units,     
 									 makesounding     = true,
-									 figsize          = (9,5))
+									 figsize          = (9,5));
