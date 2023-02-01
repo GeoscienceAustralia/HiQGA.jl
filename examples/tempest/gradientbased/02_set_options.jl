c4c0c24dbde864b718b5d3220c1085fa69f5b2a2
Anand Ray <a2ray@ucsd.edu>
2023-02-01 14:43:23 +1100

    WIP: amponly grad works with R2

--- examples/tempest/gradientbased/02_set_options.jl
+++ examples/tempest/gradientbased/02_set_options.jl
@@ -3,8 +3,8 @@
 σstart, σ0 = map(x->zeros(length(tempest.ρ)-nfixed), 1:2)
 σstart .= -2. # constant value assigned to multiple elements of array (start)
 σ0 .= -2 # constant value assigned to multiple elements of array (background)
-regtype = :R1 # :R1 is first difference regularization, :R2 is two succesive first differences
+regtype = :R2 # :R1 is first difference regularization, :R2 is two succesive first differences
 lo, hi = -3, 1 # min, max log10 conductivity
 λ²min, λ²max = -0.5, 7 # min, max of regularization parameter (Tikhonov parameter)
 ntries = 8 # how many max tries between λ²min, λ²max
-β² = 0.1 # what fraction of roughness penalty to use to enforce a penalty in deviations from background
+β² = 0.01 # what fraction of roughness penalty to use to enforce a penalty in deviations from background
