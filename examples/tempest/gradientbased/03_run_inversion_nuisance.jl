de793397bff7d2383fe760deef9a136da07d019c
Anand Ray <a2ray@ucsd.edu>
2023-02-02 19:58:10 +1100

    WIP joint, very hard. best I can do is RMS 2

--- examples/tempest/gradientbased/03_run_inversion_nuisance.jl
+++ examples/tempest/gradientbased/03_run_inversion_nuisance.jl
@@ -17,7 +17,7 @@ else
 end         
 ## do the inversion
 tempest = deepcopy(Torig);
-m, nu, χ², χ²nu, λ², idx, idxnu = transD_GP.gradientinv(σstart, σ0, nustart, tempest, nstepsmax=12, 
+m, nu, χ², χ²nu, λ², idx, idxnu = transD_GP.gradientinv(σstart, σ0, nustart, tempest, nstepsmax=16, 
                             ;nubounds, nuλ²frac, ndivsnu, ntriesnu,
                             λ²min, λ²max, β², ntries,
                             lo, hi, regtype);
