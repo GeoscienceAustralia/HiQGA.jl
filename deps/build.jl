db833bf32840503ee3bd0909b2b92993c239413c
Anand Ray <a2ray@ucsd.edu>
2023-07-22 00:18:36 +1000

    bizarre Conda error on Registrator, trying Conda.pip

--- deps/build.jl
+++ deps/build.jl
@@ -1,5 +1,7 @@
 import Pkg, Conda
 @info "building!"
+Conda.pip_interop(true)
+Conda.pip("install", "matplotlib")
 Conda.add("matplotlib")
 ENV["PYTHON"] = joinpath(Conda.ROOTENV, "bin", "python")
 Pkg.build("PyCall")
