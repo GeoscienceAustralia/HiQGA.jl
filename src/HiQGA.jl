29bcf332b7c1340db7969a1d5b0cc003d163d1c7
a2ray <52647259+a2ray@users.noreply.github.com>
2023-07-21 19:47:32 +1000

    Update HiQGA.jl to not use __init__ stuff

--- src/HiQGA.jl
+++ src/HiQGA.jl
@@ -1,16 +1,16 @@
 module HiQGA
 
-using PyCall 
-using PyPlot
+# using PyCall 
+# using PyPlot
 
-const matplotlib = PyNULL()
-const plt = PyNULL()
+# const matplotlib = PyNULL()
+# const plt = PyNULL()
 
-function __init__()
-    @info "I made it here"
-    copy!(matplotlib, pyimport_conda("matplotlib", "matplotlib", "conda-forge"))
-    copy!(plt, pyimport_conda("matplotlib.pyplot", "matplotlib", "conda-forge"))
-end
+# function __init__()
+#     @info "I made it here"
+#     copy!(matplotlib, pyimport_conda("matplotlib", "matplotlib", "conda-forge"))
+#     copy!(plt, pyimport_conda("matplotlib.pyplot", "matplotlib", "conda-forge"))
+# end
 
 module transD_GP
     include("GP.jl")
