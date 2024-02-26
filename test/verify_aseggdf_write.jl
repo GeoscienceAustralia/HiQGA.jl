55945e08d5c641cd3bd5e6291c5342a08a11cec9
rade7 <rd8735@gadi-cpu-bdw-0008.gadi.nci.org.au>
2024-02-26 15:05:02 +1100

    made changes to writing functions: -split functions for .dat and .dfn file -created fortran formatter for field codes -created a test file

--- test/verify_aseggdf_write.jl
+++ test/verify_aseggdf_write.jl
@@ -1,6 +1,7 @@
 using HiQGA
 vall=[[1, [2., 3.]], [2, [3., 4.]]]
-sfmt = ["%3d", "%4.1f"]
+sfmt = ["%3i", "%4.1f"]
 outfile = "myfile"
-channel_names = [["FID", "dBz/dt"], ["","pV/Am4"],["FID", "dBz/dt"]]
+channel_names = [["FID", "dBzdt"], ["","pV/Am4"],["FID", "dBz/dt"]]
 transD_GP.CommonToAll.writeaseggdf(vall, sfmt, outfile, channel_names)
+transD_GP.CommonToAll.writedfn(vall, channel_names, sfmt, outfile)
