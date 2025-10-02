e300aab3bec5383d55bdcd98aa0ebd792f96a530
Anandaroop Ray <a2ray@ucsd.edu>
2025-10-02 11:04:08 +1000

    with disba error handling, runs

--- examples/SWD/forward_examples/04_b_make_inversion_opts.jl
+++ examples/SWD/forward_examples/04_b_make_inversion_opts.jl
@@ -1,23 +1,24 @@
 ## make options for the stationary GP
 fileprefix = "SWD_test_"
-K = transD_GP.GP.OrstUhn()
-nmin, nmax = 2, 40
-fbounds = [minimum(vs) maximum(vs)]
-sdev_pos = [0.05abs(diff([extrema(znall)...])[1])]
-sdev_prop = 0.07*diff(fbounds, dims=2)[:]
-demean = false
-sdev_dc = 0.008*diff(fbounds, dims=2)[:]
-sampledc = true
-xall = permutedims(collect(znall))
-xbounds = permutedims([extrema(znall)...])
-λ, δ = [5], 0.1
+K = transD_GP.GP.OrstUhn() # GP Kernel type, OrstUhn is L1 sharp
+nmin, nmax = 2, 40 # no. of GP nuclei
+fbounds = [0.9minimum(vs) 1.1maximum(vs)] # property bounds, n_property × 2 array
+xall = permutedims(collect(znall)) # spatial locations to interpolate GP at
+xbounds = permutedims([extrema(znall)...]) # spatial bounds, n_spatial_dims × 2 array
+sdev_pos = [0.05abs(diff([extrema(znall)...])[1])] # perturbation for position, n_spatial_dims × 2 array
+sdev_prop = 0.07*diff(fbounds, dims=2)[:] # perturbation for property, n_property × 2 array
+sdev_dc = 0.008*diff(fbounds, dims=2)[:] # DC perturbation for property, n_property × 2 array
+λ, δ = [5.5], 0.1 # correlation length, nugget
 ## Initialize a stationary GP using these options
 using Random
 Random.seed!(12)
 opt = transD_GP.OptionsStat(;nmin, nmax, xbounds, fbounds,
                         xall, λ, δ, sdev_prop, sdev_pos,
                         demean, sampledc, sdev_dc, K,
-                        quasimultid = false, save_freq = 50, fdataname = fileprefix,
+                        quasimultid = false, 
+                        save_freq = 25, 
+                        stat_window = 200,
+                        fdataname = fileprefix,
                         )
 ## 
 transD_GP.get_misfit(vs, opt, swd)
--- examples/SWD/forward_examples/04_c_synthetic_run_McMC.jl
+++ examples/SWD/forward_examples/04_c_synthetic_run_McMC.jl
@@ -1,7 +1,7 @@
 # set up McMC using 4 chains
 using Distributed
-nsamples, nchains, nchainsatone = 20001, 4, 1
-Tmax = 2.50
+nsamples, nchains, nchainsatone = 50001, 6, 1
+Tmax = 2.2
 addprocs(nchains)
 @info "workers are $(workers())"
 @everywhere begin
@@ -14,7 +14,3 @@ end
 ## run McMC - sample a uniform prior between 0 and 1 at depths between 0 and 1 with 5 nuclei
 @time transD_GP.main(opt, swd, Tmax=Tmax, nsamples=nsamples, nchains=nchains, nchainsatone=nchainsatone)
 rmprocs(workers())
-## plot
-transD_GP.getchi2forall(opt, fsize=8, alpha=0.5)
-opt.xall[:] = zall
-transD_GP.plot_posterior(swd, opt,  burninfrac=0.5, figsize=(4,4), fsize=8, nbins=50, usekde=true, cmappdf = "bone_r")
