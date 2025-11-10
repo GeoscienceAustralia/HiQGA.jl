f35d5641a7a7d5c8363f8de6bf8e151b0afd2e7c
Anandaroop Ray <a2ray@ucsd.edu>
2025-11-10 12:56:25 +1100

    trying without a build.jl again

--- deps/build.jl
+++
@@ -1,8 +0,0 @@
-import Pkg, Conda
-@info "building!"
-Conda.pip_interop(true)
-Conda.pip("install", "matplotlib==3.7.3")
-Conda.add("matplotlib")
-ENV["PYTHON"] = joinpath(Conda.ROOTENV, "bin", "python")
-Pkg.build("PyCall")
-@info "built PyCall!"
