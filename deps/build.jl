e29ed75de37f0cffbea94d836c79f89ba2618853
Anandaroop Ray <a2ray@ucsd.edu>
2025-11-10 13:18:31 +1100

    more Conda adjustments

---
+++ deps/build.jl
@@ -0,0 +1,8 @@
+import Pkg, Conda
+@info "building!"
+#Conda.pip_interop(true)
+#Conda.pip("install", "matplotlib==3.7.3")
+Conda.add("matplotlib")
+ENV["PYTHON"] = joinpath(Conda.ROOTENV, "bin", "python")
+Pkg.build("PyCall")
+@info "built PyCall!"
