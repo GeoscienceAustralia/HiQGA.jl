9ef0113f42fd4d1e09a116e4b8ac215e03d92627
Timothy Scarr <ts7941@gadi-cpu-bdw-0007.gadi.nci.org.au>
2026-01-13 12:45:07 +1100

    WIP: Conda errors removed

--- deps/build.jl
+++ deps/build.jl
@@ -1,7 +1,7 @@
 import Pkg, Conda
 @info "building!"
-Conda.pip_interop(true)
-Conda.pip("install", "matplotlib==3.7.3")
+#Conda.pip_interop(true)
+#Conda.pip("install", "matplotlib==3.7.3")
 Conda.add("matplotlib")
 ENV["PYTHON"] = joinpath(Conda.ROOTENV, "bin", "python")
 Pkg.build("PyCall")
